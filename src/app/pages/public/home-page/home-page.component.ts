6f27f13a79131e7ba5db729e66b3286ba6147ea7
Mickael <desbiens.mickael01@gmail.com>
2024-06-15 15:51:20 +0200

    animation added to title

--- src/app/pages/public/home-page/home-page.component.ts
+++ src/app/pages/public/home-page/home-page.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
-  test = 'test';
+  title: string[] = 'Bienvenue sur '.split('');
+  appName = 'Food Buddy';
 }
