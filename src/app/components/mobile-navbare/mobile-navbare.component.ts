6b9133b255a38c73f3861214624dd85448446b2a
Mickael <desbiens.mickael01@gmail.com>
2024-06-15 10:44:44 +0200

    Mobile navbar structure

---
+++ src/app/components/mobile-navbare/mobile-navbare.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-navbare',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-navbare.component.html',
+  styleUrl: './mobile-navbare.component.css',
+})
+export class MobileNavbareComponent {
+  test = 'test';
+}
--- src/app/layouts/layout-base/layout-base.component.ts
+++ src/app/layouts/layout-base/layout-base.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MobileNavbareComponent } from '../../components/mobile-navbare/mobile-navbare.component';
 import { NavbareComponent } from '../../components/navbare/navbare.component';
 
 @Component({
   selector: 'app-layout-base',
   standalone: true,
-  imports: [RouterOutlet, NavbareComponent],
+  imports: [RouterOutlet, NavbareComponent, MobileNavbareComponent],
   templateUrl: './layout-base.component.html',
   styleUrl: './layout-base.component.css',
 })
