818ca25698b6c0865f3387c59d85ef96f0be888e
AlicePhkeo <aphouangkeo@gmail.com>
2024-06-19 17:24:40 +0200

    i did the footer with Mickael

--- src/app/layouts/layout-base/layout-base.component.ts
+++ src/app/layouts/layout-base/layout-base.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MobileNavbareComponent } from '../../components/navbar/mobile-navbare/mobile-navbare.component';
 import { NavbareDesktopComponent } from '../../components/navbar/navbare-desktop/navbare.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-layout-base',
   standalone: true,
-  imports: [RouterOutlet, NavbareDesktopComponent, MobileNavbareComponent],
+  imports: [
+    RouterOutlet,
+    NavbareDesktopComponent,
+    MobileNavbareComponent,
+    FooterComponent,
+  ],
   templateUrl: './layout-base.component.html',
   styleUrl: './layout-base.component.css',
 })
