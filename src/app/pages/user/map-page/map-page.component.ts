fc0b712f2706b7980cde1720b5fd0b7b21dfad75
Mickael <desbiens.mickael01@gmail.com>
2024-07-01 11:19:10 +0200

    install leaflet

---
+++ src/app/pages/user/map-page/map-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-page.component.html',
+  styleUrl: './map-page.component.css',
+})
+export class MapPageComponent {
+  test = 'test';
+}
