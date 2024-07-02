a1bb7d11211e9aaba2fbd9e6a41a71047d08c96d
Mickael <desbiens.mickael01@gmail.com>
2024-07-02 17:25:59 +0200

    creation and implementation of the map

--- src/app/pages/user/map-page/map-page.component.ts
+++ src/app/pages/user/map-page/map-page.component.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import {
+import L, {
   Icon,
   LatLngExpression,
-  Layer,
   Map,
   MapOptions,
   Marker,
@@ -13,6 +12,7 @@ import {
   marker,
   tileLayer,
 } from 'leaflet';
+import 'leaflet-routing-machine';
 
 @Component({
   selector: 'app-map-page',
@@ -26,10 +26,11 @@ export class MapPageComponent implements OnInit {
   markers: Marker[] = [];
   userMarker!: Marker;
   userIcon!: Icon;
+  merchantIcon!: Icon;
   map!: Map;
-  layers: Layer[] = [];
 
   ngOnInit() {
+    // options de la carte
     this.options = {
       layers: [
         tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -37,10 +38,11 @@ export class MapPageComponent implements OnInit {
           attribution: '© OpenStreetMap contributors',
         }),
       ],
-      zoom: 18,
+      zoom: 16,
       center: latLng(50.528256, 2.9523968),
     };
 
+    // icone utilisateur
     this.userIcon = icon({
       ...Icon.Default.prototype.options,
       iconUrl: 'assets/marker-icon.png',
@@ -48,7 +50,54 @@ export class MapPageComponent implements OnInit {
       shadowUrl: 'assets/marker-shadow.png',
     });
 
-    // Créer un contrôle personnalisé pour le bouton de localisation
+    // icone commerçant
+    this.merchantIcon = icon({
+      ...Icon.Default.prototype.options,
+      iconSize: [35, 50],
+      iconAnchor: [17, 50],
+      popupAnchor: [1, -34],
+      iconUrl: 'assets/marker-merchant.png',
+      iconRetinaUrl: 'assets/marker-merchant.png',
+      shadowUrl: 'assets/marker-shadow.png',
+    });
+  }
+
+  // initialisation de la carte quand la page est chargée
+  onMapReady(map: Map) {
+    this.map = map;
+
+    // recuperation de la position de l'utilisateur
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const coords = position.coords;
+
+        // Ajoutet l'utilisateur sur la carte
+        this.userMarker = marker([coords.latitude, coords.longitude], {
+          title: 'Votre position',
+          icon: this.userIcon,
+        });
+
+        // Ajout du marqueur commerçant avec une popup
+        // TODO : modifier pour ajouter tous les commerçants
+        const test = marker(latLng(50.6311065, 3.0195457), {
+          title: 'Votre position',
+          icon: this.merchantIcon,
+        }).bindPopup(
+          '<div><img src="assets/default-busines.png"alt="map-marker" /> <a href="#" class="mt-4">Voir les produits</a></div>',
+        );
+
+        // Ajout des marqueurs sur la carte
+        this.markers.push(test);
+        this.markers.push(this.userMarker);
+
+        // setView sert à centrer la carte sur la position de l'utilisateur
+        map.setView(latLng(coords.latitude, coords.longitude), 16);
+      },
+      error => {
+        console.error('Geolocation error: ', error);
+      },
+      { enableHighAccuracy: true },
+    );
   }
 
   // pour ce déplcer sur l'emplacement de l'utilisateur
@@ -70,25 +119,41 @@ export class MapPageComponent implements OnInit {
     });
   }
 
-  onMapReady(map: Map) {
-    this.map = map;
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const coords = position.coords;
-        // Ajoutet l'utilisateur sur la carte
-        this.userMarker = marker([coords.latitude, coords.longitude], {
-          title: 'Votre position',
-          icon: this.userIcon,
-        });
-        this.markers.push(this.userMarker);
+  //pour tracer le chemin de la route à partir de l'utilisateur jusqu'à la destination
+  onTraceBusiness() {
+    const destination = latLng(50.6311065, 3.0195457);
 
-        // setView sert à centrer la carte sur la position de l'utilisateur
-        map.setView(latLng(coords.latitude, coords.longitude), 18);
+    // Ajouter le contrôle de routage
+    L.Routing.control({
+      // Désactive les marqueurs de l'itinéraire puisque nous avons notre propre marqueur
+      plan: L.Routing.plan([this.userMarker.getLatLng(), destination], {
+        createMarker: () => {
+          return false;
+        },
+        addWaypoints: false,
+      }),
+      waypoints: [this.userMarker.getLatLng(), destination],
+      routeWhileDragging: false,
+      show: false,
+      // style du tracé
+      lineOptions: {
+        styles: [{ color: 'blue', opacity: 0.6, weight: 4 }],
+        addWaypoints: true,
+        extendToWaypoints: true,
+        missingRouteTolerance: 10,
       },
-      error => {
-        console.error('Geolocation error: ', error);
-      },
-      { enableHighAccuracy: true },
-    );
+    })
+      .on('routesfound', () => {
+        // Rechercher et ouvrir le popup du marqueur de destination après la fin du routage
+        this.map.eachLayer(layer => {
+          if (layer instanceof L.Marker) {
+            const markerLatLng = layer.getLatLng();
+            if (markerLatLng.equals(destination)) {
+              layer.openPopup();
+            }
+          }
+        });
+      })
+      .addTo(this.map);
   }
 }
