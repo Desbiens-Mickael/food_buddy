a6cbc8733d19f058b3ff19bac4c498d2f94bd67d
AlicePhkeo <aphouangkeo@gmail.com>
2024-07-16 14:28:57 +0200

    css reservation done

--- src/app/components/card-reservation/card-reservation.component.ts
+++ src/app/components/card-reservation/card-reservation.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-reservation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-reservation.component.html',
   styleUrl: './card-reservation.component.css',
 })
-export class CardReservationComponent {}
+export class CardReservationComponent {
+  isHidden = false;
+
+  toggle() {
+    this.isHidden = !this.isHidden;
+  }
+}
